42227e821c93e5dd1413fb11d386e0d5da19571e
Brandon Pollack <brandonpollack23@gmail.com>
2024-02-23 09:56:53 -0800

    fixed a ton of stuff

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO
+
+## General functionality
+
+* Copy code button in top right of CodeBlocks (wrapper around prism in general)
+
+## Posts
+
+* Kanji draw that draws all lines at once
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,21 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
+import rehypeToc from "rehype-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), solidJs({ include: ["**/solid/**"] })],
+  integrations: [
+    mdx({
+      gfm: true,
+      syntaxHighlight: "prism",
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeToc],
+    }),
+    sitemap(),
+    tailwind(),
+    solidJs({ include: ["**/solid/**"] }),
+  ],
 });
